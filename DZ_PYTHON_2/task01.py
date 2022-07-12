02477ac7b4dff800f0143cb62879f575493b0f9b
ZhekaProff <Zhekakad@ya.ru>
2022-07-12 17:59:59 +0900

    решение первой задач

--- DZ_PYTHON_2/task01.py
+++ DZ_PYTHON_2/task01.py
@@ -6,5 +6,37 @@ def sum_of_digits(a):
         result += a % 10
         a //= 10
     return result
- 
-print(sum_of_digits(123))
+''' Функция по работе с int
+
+'''
+
+# разделим введённое (тип данных строка) на две части
+def float_namber(num: any):
+    x = num.split(".") 
+    a = int(x[0]) # целая часть
+    b = int(x[1]) # дробная часть
+    mult = 0
+    while (a > 0): # перемножаем числа целой части
+        mult = mult + (a % 10)
+        a = a // 10
+    while (b > 0): 
+        mult = mult + (b % 10)
+        b = b // 10
+    return mult
+''' Функция по работе с float
+
+'''
+# Я хотел сделать две функции для int и float, и потом определять число и направлять в нужную функцию, но реализовать не смог. Возмодно ли это? 
+
+# Решеие задачи через строку
+
+def float_namber(number: any):
+    summa = 0
+    num = str(number).replace(".", "")
+    for index in num:
+        summa += int(index)
+    return summa
+
+number = input("Enter your number? ")
+print(f'{number} ->', float_namber(number))
+
