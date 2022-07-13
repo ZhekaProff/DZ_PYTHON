ec198ffc7590311c7def621b86d3e543243e08cb
ZhekaProff <Zhekakad@ya.ru>
2022-07-13 12:58:51 +0900

    решение третьей задачи

--- DZ_PYTHON_2/task03.py
+++ DZ_PYTHON_2/task03.py
@@ -1,10 +1,12 @@
 # Задайте список из n чисел последовательности (1 + 1/n)^n и выведите на экран их сумму.
 
-def metod_formula(n):
-    summa = 0
-    for i in range(n):
-        summa += (1 + 1/n)**n
-        print(round(summa), end=(" "))
-
 n = int(input('Введите число: '))
-metod_formula(n)
+result = 0
+count = 0
+summ = 0
+for i in range(1, n +1 ):
+    result = (1 + 1/i)**i
+    count += 1
+    print(f'{count}: {round(result)}', end=', ')
+    summ += result
+print(f'\nсумма числел {round(summ)}')
